731d2b7c8e7c2806fce0c913e9ad406913ef6ba5
김성식 <128824367+sungsik-git@users.noreply.github.com>
2023-11-30 15:23:45 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+        var answer: Int = 0
+        if(number % n == 0 && number % m == 0) answer = 1
+        return answer
+    }
+}
